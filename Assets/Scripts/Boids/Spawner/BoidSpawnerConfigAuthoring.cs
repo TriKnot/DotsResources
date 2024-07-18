34676b6004881dc59648f8691e79252da98009e5
TriKnot <90341216+TriKnot@users.noreply.github.com>
2024-07-18 14:06:49 +0200

    Spawn Boids

---
+++ Assets/Scripts/Boids/Spawner/BoidSpawnerConfigAuthoring.cs
@@ -0,0 +1,38 @@
+﻿using Unity.Burst;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace Biods.Spawner
+{
+    [BurstCompile]
+    public struct BoidSpawnerConfig : IComponentData
+    {
+        public Entity Prefab;
+        public int Count;
+        public float3 Bounds;
+    }
+    
+    [BurstCompile]
+    public class BoidSpawnerConfigAuthoring : MonoBehaviour
+    {
+        public GameObject Prefab;
+        public int Count;
+        public float3 Bounds;
+
+        public class BoidSpawnerBaker : Baker<BoidSpawnerConfigAuthoring>
+        {
+            public override void Bake(BoidSpawnerConfigAuthoring configAuthoring)
+            {
+                var entity = GetEntity(TransformUsageFlags.Dynamic);
+                AddComponent(entity,
+                    new BoidSpawnerConfig
+                    {
+                        Prefab = GetEntity(configAuthoring.Prefab, TransformUsageFlags.Dynamic), 
+                        Count = configAuthoring.Count,
+                        Bounds = configAuthoring.Bounds
+                    });
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Boids/Spawner/BoidSpawnerSystem.cs
@@ -0,0 +1,44 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+namespace Biods.Spawner
+{
+    [BurstCompile]
+    public partial struct BoidSpawnerSystem : ISystem
+    {
+        public void OnCreate(ref SystemState state)
+        {
+            state.RequireForUpdate<BoidSpawnerConfig>();   
+        }
+        
+        public void OnUpdate(ref SystemState state)
+        {
+            state.Enabled = false;
+            
+            EntityManager entityManager = state.EntityManager;
+            EntityCommandBuffer commandBuffer = new EntityCommandBuffer(Allocator.Temp);
+            
+            BoidSpawnerConfig boidSpawnerConfig = SystemAPI.GetSingleton<BoidSpawnerConfig>();
+            
+            for (int i = 0; i < boidSpawnerConfig.Count; i++)
+            {
+                Entity entity = commandBuffer.Instantiate(boidSpawnerConfig.Prefab);
+                commandBuffer.SetComponent(entity, new LocalTransform
+                {
+                    Position = new float3(
+                        UnityEngine.Random.Range(-boidSpawnerConfig.Bounds.x, boidSpawnerConfig.Bounds.x),
+                        UnityEngine.Random.Range(-boidSpawnerConfig.Bounds.y, boidSpawnerConfig.Bounds.y),
+                        UnityEngine.Random.Range(-boidSpawnerConfig.Bounds.z, boidSpawnerConfig.Bounds.z)
+                        ),
+                    Rotation = quaternion.identity,
+                    Scale = 1f
+                });
+            }
+            
+            commandBuffer.Playback(entityManager);
+        }
+    }
+}
